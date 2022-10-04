e29ca84e1c8e7434d095258997a2770043c74eb5
Dae17 <dejontramnell@gmail.com>
2022-10-04 12:26:42 -0700

    made many to one relation with items and users.

--- apps/listing/models.py
+++ apps/listing/models.py
@@ -1,8 +1,12 @@
 # Create your models here.
 
 from django.db import models
+from django.contrib.auth import get_user_model
+
 
 class item(models.Model):
+    owner = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
     item_name = models.CharField(max_length=120)
     price = models.IntegerField()
     item_discription = models.TextField()
