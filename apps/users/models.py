5eb329ece6ee99b33e447e77ea397f6f5550219f
Dae17 <dejontramnell@gmail.com>
2022-10-04 12:23:49 -0700

    Created user auth forms.

--- apps/users/models.py
+++ apps/users/models.py
@@ -7,11 +7,4 @@ from datetime import date
 
 
 class User(AbstractUser):
-  username = models.CharField(max_length = 50, blank = True, null = True, unique = True)
-  email = models.EmailField(_('email address'), unique = True)
-  native_name = models.CharField(max_length = 5)
-  phone_no = models.CharField(max_length = 10)
-  USERNAME_FIELD = 'email'
-  REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
-  def __str__(self):
-      return "{}".format(self.email)
+  pass
--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,11 +1,8 @@
 from django.urls import path, include
 
 from . import views
-from django.views.generic.base import TemplateView
 
 urlpatterns = [
-   path('', views.index, name = 'index'),
-   path('index/', views.index, name='index' ),
+   path('register/', views.register_request, name='register' ),
    path('login/', views.login_request, name='login'),
-   path('listing/', include("apps.listing.urls"))
 ]
