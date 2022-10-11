4c3b7e2770b54ae8ce0f467c9b9705cbf43f4499
Dae17 <dejontramnell@gmail.com>
2022-10-11 14:40:01 -0700

    Namespaced user urls.

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -2,6 +2,8 @@ from django.urls import path, include
 
 from . import views
 
+app_name = "users"
+
 urlpatterns = [
    path('register/', views.register_request, name='register' ),
    path('login/', views.login_request, name='login'),
