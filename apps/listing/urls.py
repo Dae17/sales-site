94e6282149145a6928bd88178d95207efe0f7d48
shane chavez <shane.chavez74@gmail.com>
2022-11-10 12:41:02 -0800

    Started Search Bar

--- apps/listing/urls.py
+++ apps/listing/urls.py
@@ -2,10 +2,13 @@ from django.urls import path
 
 from . import views
 
+from .views import  SearchResultsView
+
 app_name = "listing"
 
 urlpatterns = [
     path('', views.list, name='list'),
     path('listing/<pk>/', views.ItemDetailView.as_view(), name='detail'),
     path('create', views.ItemCreateView.as_view(), name = "create"),
+    path("search/", views.SearchResultsView.as_view(), name ="search_results"),
 ]
--- apps/listing/views.py
+++ apps/listing/views.py
@@ -2,6 +2,8 @@ import operator
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from django.db.models import Q
+
 from .models import item
 
 from django.views.generic.edit import CreateView
@@ -9,6 +11,8 @@ from django.views.generic.edit import CreateView
 from django.views.generic.detail import DetailView
 
 from django.contrib.messages.views import SuccessMessageMixin
+
+from django.views.generic import TemplateView, ListView
 
 
 
@@ -34,4 +38,15 @@ class ItemCreateView(SuccessMessageMixin, CreateView):
         listing.owner = self.request.user
         listing.save()
         return super().form_valid(form)
-    
+
+
+class SearchResultsView(ListView):
+    model = item
+    template_name = 'search_results.html'
+
+    def get_queryset(self):  # new
+        query = self.request.GET.get("q")
+        object_list = City.objects.filter(
+            Q(name__icontains=query) 
+        )
+        return object_list
