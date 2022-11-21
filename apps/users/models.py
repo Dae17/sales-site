b876c98eb4ac644a30dd230fc61ff67a8da48a4d
shane chavez <shane.chavez74@gmail.com>
2022-11-21 11:18:57 -0800

    Owner in Item card

--- apps/users/models.py
+++ apps/users/models.py
@@ -13,7 +13,7 @@ class User(AbstractUser):
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
-    avatar = models.ImageField(default='default.jpg', upload_to='profile_images')
+    avatar = models.ImageField(default='default.png', upload_to='profile_images')
     bio = models.TextField()
 
     def __str__(self):
