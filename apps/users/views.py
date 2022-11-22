aa809708240a2f8a9069d037784c4e82663a0195
shane chavez <shane.chavez74@gmail.com>
2022-11-22 12:44:51 -0800

    Fixed Link issues

--- apps/users/views.py
+++ apps/users/views.py
@@ -13,7 +13,7 @@ def register_request(request):
             user = form.save()
             login(request, user)
             messages.success(request, "Registration successful.")
-            return redirect("main:homepage")
+            return redirect("apps:listing:list")
         messages.error(request, "Unsuccessful registration. Invalid information.")
     form = NewUserForm()
     return render(
